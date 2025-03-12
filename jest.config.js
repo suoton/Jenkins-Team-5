77fd1d034ed431d500cbe3cad6cae2bf0bbe29bb
suoton <120308655+suoton@users.noreply.github.com>
2025-03-12 17:16:52 +0100

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}','test/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>/test/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/test/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)':
+      '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  reporters: [
+    'default',
+    [
+      '<rootDir>/index.js',
+      {
+        publicPath: './html-report',
+        filename: 'report.html',
+        enableMergeData: true,
+        dataMergeLevel: 2,
+        openReport: true,
+        inlineSource: true,
+        darkTheme: true,
+        stripSkippedTest: true,
+      },
+    ],
+  ],
+};
